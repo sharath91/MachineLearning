360c7779ea35cf6b8bab70733f13d9035338a2f0
SharathReddy <m.sharathiitg02@gmail.com>
2016-02-12 19:03:18 +0530

    added the data sources

---
+++ DataCollection/UCI-data/Dataanalysis.m
@@ -0,0 +1,7 @@
+%Machine Learning Diabetic classifier project
+
+% load the pima indians data and plot them against whether they are diabetic or not
+% this will give us whether or not a specific feature plotted against have direct relationship or not
+
+
+
