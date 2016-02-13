8e42c951ef922a87922f7843c90262276a7a8299
SharathReddy <m.sharathiitg02@gmail.com>
2016-02-13 16:43:03 +0530

    added analysis for four features

--- DataCollection/UCI-data/Dataanalysis.m
+++ DataCollection/UCI-data/Dataanalysis.m
@@ -14,14 +14,92 @@ p = size_A(1);
 
 
 A(:,1);
-plot(1,3);
+%plot(1,3);
 %sample visualization of no of times pregnant vs being diabetic
-for i=1:p
-	if(A(p,9) == 1)
-		plot(A(p,1),A(p,9),"+");
-		axis([0 20 0 3]);
-		hold on;
+
+%for i=1:p
+%	if(A(i,9) == 1)
+%		plot(A(i,1),A(i,9),"+");
+%		axis([0 20 0 3]);
+%		hold on;
+%	else
+%		plot(A(i,1),A(i,9),"o");	
+%		axis([0 20 0 3]);
+%		hold on;
+		
+%xlabel('no of times pregnant');
+%ylabel('tested +ve or -ve for diabetic');
+%hold off;
 			
+%end
+%end
+
+
+
+%  Plasma glucose concentration a 2 hours in an oral glucose tolerance test vs being diabetic
+
+max(A(:,2));
+min(A(:,2));
+
+%for i=1:p
+%	if(A(i,9) == 1)
+%		plot(A(i,2),A(i,9),"+");
+%		axis([0 300 0 3]);
+%		hold on;
+%	else
+%		plot(A(i,2),A(i,9),"o");	
+%		axis([0 300 0 3]);
+%		hold on;
+
+%xlabel('Plasma glucose concentration');
+%ylabel('tested +ve or -ve for diabetic');
+%hold off;
+
+%end
+%end
+
+% diastolic blood pressure vs being diabetic
+
+max(A(:,3));
+min(A(:,3));
+
+%for i=1:p
+%	if(A(i,9) == 1)
+%		plot(A(i,3),A(i,9),"+");
+%		axis([0 150 0 3]);
+%		hold on;
+
+%	else
+%		plot(A(i,3),A(i,9),"o");	
+%		axis([0 150 0 3]);
+%		hold on;
+
+%xlabel('diastolic blood pressure');
+%ylabel('tested +ve or -ve for diabetic');
+%hold off;
+
+%end
+%end
+
+max(A(:,4));
+min(A(:,4));
+
+for i=1:p
+	if(A(i,9) == 1)
+		plot(A(i,4),A(i,9),"+");
+		axis([0 150 0 3]);
+		hold on;
+
+	else
+		plot(A(i,4),A(i,9),"o");	
+		axis([0 150 0 3]);
+		hold on;
+
+xlabel('Triceps skin fold thickness (mm)');
+ylabel('tested +ve or -ve for diabetic');
+
+
 end
 end
+
 
